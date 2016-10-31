d498c03823c7b1bb1e47685adf45e18428e03cc3
erniecho <erniecho@gmail.com>
2016-10-30 22:05:31 -0400

    Added a new file using Command + N to create a new file for view control. changing the customs class using Command + Option + 3..

---
+++ WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Ernie Cho on 10/30/16.
+//  Copyright © 2016 ErnShu. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+    
+}
