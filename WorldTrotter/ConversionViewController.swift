6c3be64bc7cf70fdb0f978bb8f56fad76b5950f4
erniecho <erniecho@gmail.com>
2016-10-31 01:20:37 -0400

    added number format to code.

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -13,7 +13,7 @@ class ConversionViewController: UIViewController {
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textField: UITextField!
     
-    var fahrenheitValue: Double?
+  //  var fahrenheitValue: Double?
     
     var celsiusValue: Double? {
         if let value = fahrenheitValue {
@@ -24,18 +24,45 @@ class ConversionViewController: UIViewController {
         }
     }
     
+    func updateCelsiusLabel() {
+        if let value = celsiusValue {
+            celsiusLabel.text = numberFormatter.stringFromNumber(value)
+        }
+        else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    var fahrenheitValue: Double? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .DecimalStyle
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }
+    
     
     @IBAction func fahreheitFieldEditingChanged(textField: UITextField) {
-        if let text = textField.text , !text.isEmpty {
-            celsiusLabel.text = text
+        if let text = textField.text , let value = Double(text) {
+            fahrenheitValue = value
         }
         else {
             
         }
-        celsiusLabel.text = "???"
+        fahrenheitValue = nil
     }
+    
+    
     
     @IBAction func dismissKeyboard(sender: AnyObject) {
         textField.resignFirstResponder()
     }
+    
+    
 }
